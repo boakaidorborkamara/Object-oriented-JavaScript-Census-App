d62f3c27f7507853e33451e802a1513f47591131
Boakai Dorbor Kamara <boakaidkamara@gmail.com>
2021-07-18 23:25:52 +0000

    Implementing OOP in JS

--- script/script.js
+++ script/script.js
@@ -1,8 +1,29 @@
-fetch("./json/census.json")
-.then(res => res.json())
-.then((data)=>{
-    console.log(data);
-})
-.catch((err)=>{
-    console.log(err);
-})
+
+let census ={
+    //get all the data from the json file
+    getAllData: function(){
+        fetch("./json/census.json")
+        .then(res => res.json())
+        .then((data)=>{
+            console.log(data);
+            return data;
+        })
+        .catch((err)=>{
+            console.log(err);
+        })
+    },
+
+    // calculateTotalPopulation(population_data){
+
+    // },
+
+    //feed the html with data
+    feedAppWithData(){
+        let census_data = census.getAllData();
+    }
+
+}
+
+census.feedAppWithData();
+
+
